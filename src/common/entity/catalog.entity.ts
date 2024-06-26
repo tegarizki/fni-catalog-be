02e62c87e5a2d545a9e8017c9721ceb4e9412cd0
Tegar Rizki Harnandi <tegarharnandi@gmail.com>
2024-06-26 10:08:40 +0700

    change nullable catalog entity

--- src/common/entity/catalog.entity.ts
+++ src/common/entity/catalog.entity.ts
@@ -5,18 +5,18 @@ export class CatalogEntity{
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column({ name: 'product_name'})
+    @Column({ name: 'product_name', nullable: true})
     productName: string;
 
-    @Column({ name: 'vendor'})
+    @Column({ name: 'vendor', nullable: true})
     vendor: string;
 
-    @Column({ name: 'type_radio'})
+    @Column({ name: 'type_radio', nullable: true})
     typeRadio: string;
 
-    @Column({ name: 'id_detail_product'})
+    @Column({ name: 'id_detail_product', nullable: true})
     idDetailProduct: number;  
 
-    @Column({ name: 'image_name'})
+    @Column({ name: 'image_name', nullable: true})
     imageName: string;
 }
